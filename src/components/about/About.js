18739b3b50450dc075bfc984a3fe9dee5d2cdb78
gwebb17 <gwebb1723@gmail.com>
2023-07-29 14:07:43 -0700

    about text into separate divs

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -20,22 +20,23 @@ const About = () => {
                         <a href="https://funkyouter.space"><img src={process.env.PUBLIC_URL + "/something"} alt='FOS logo'/></a>
                     </div>
                 </div>
-            </div>        
-            
-            <h2 className='about__header_text'>
-                ABOUT
-            </h2>
-            <p className='about__text'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Vestibulum sed arcu non odio euismod lacinia. Nunc vel risus commodo viverra maecenas accumsan lacus. Nunc id cursus metus aliquam. 
-                Ac odio tempor orci dapibus ultrices in iaculis nunc. Lectus sit amet est placerat. Sed tempus urna et pharetra pharetra massa massa. 
-                Fermentum iaculis eu non diam phasellus vestibulum. Dolor sit amet consectetur adipiscing elit duis tristique. Sed augue lacus viverra 
-                vitae congue eu consequat ac felis. Tempor nec feugiat nisl pretium. Diam maecenas ultricies mi eget mauris pharetra et ultrices. Id 
-                ornare arcu odio ut sem nulla. Nam aliquam sem et tortor consequat id. Morbi enim nunc faucibus a pellentesque. Sem nulla pharetra diam sit. 
-                Aliquam nulla facilisi cras fermentum odio eu feugiat. Aliquet lectus proin nibh nisl condimentum id. Ac feugiat sed lectus vestibulum mattis 
-                ullamcorper. Gravida cum sociis natoque penatibus et. Risus ultricies tristique nulla aliquet enim tortor at. Arcu bibendum at varius vel 
-                pharetra. Pellentesque habitant morbi tristique senectus et.
-            </p>
+            </div>
+            <div className='about__second_row_container'>
+                <h2 className='about__header_text'>
+                    ABOUT
+                </h2>
+                <div className='about__left_text_container'>
+                    <p className='p_text_line_one'>
+                        I've got a lifelong love of creating and learning
+                    </p>
+                    < p className='p_text_line_two'>
+                        This manifests in the form of building websites or creating music
+                    </p>
+                    <p className='p_text_line_three'>
+                        Currently a full-stack developer for <a href="https://funkyouter.space"></a>Funky Outer Space
+                    </p>
+                </div>
+            </div>                  
         </div>
     );
 }
