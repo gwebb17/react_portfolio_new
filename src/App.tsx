d827237341e164d002e00fdfcf2141b31050c2d5
gwebb17 <gwebb1723@gmail.com>
2023-08-05 11:37:43 -0700

    removed swiper app working again

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,28 @@
 import React from 'react';
+import { useEffect } from 'react';
 import './App.css';
 import Nav from 'components/navbar/Nav';
 import About from 'components/about/About';
 import Resume from 'components/resume/Resume';
-import Swiper from 'swiper';
-import SwiperSlide from 'swiper/react';
-import { Navigation, Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-//*Initialize Swiper*//
-const swiper = new Swiper('.swiper',{
-  modules: [Navigation, Pagination],  //configure swiper to use these modules
-  direction: 'horizontal',
-  loop: true,  //true makes page scroll infinite
-
-  spaceBetween: 0,
-  slidesPerView: 1,
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
 
 function App() {
-    return (
-      <>
-        <div className='app__nav'>
-          <Nav />     
-        </div>
-        <div className='swiper'>
-          <div className='swiper-wrapper'>
-            <div className='app__about_container swiper-slide slide_1'>
-                <div className='app__about'>
-                  <About />
-                </div>
-            </div>
-            <div className='app__resume_container swiper-slide slide_2'>
-                <div className='app__resume'>
-                    <Resume />
-                </div>
-            </div>
+  return (
+    <>
+      <div className='app__nav'>
+        <Nav />     
+      </div>
+      <div className='app__about_container'>
+          <div className='app__about'>
+            <About />
           </div>
-          <div className='swiper-pagination'></div>
-          <div className='swiper-button-prev'></div>
-          <div className='swiper-button-next'></div>
-          <div className='swiper-scrollbar'></div>
-        </div>
-      </>
-    );
+      </div>
+      <div className='app__resume_container'>
+          <div className='app__resume'>
+              <Resume />
+          </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
